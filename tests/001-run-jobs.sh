667e3af944e4f1743f872efae61b27dca62b0f1c
sirdavenumber1 <119961113+sirdavenumber1@users.noreply.github.com>
2023-01-10 13:46:44 -0500

    Update 001-run-jobs.sh

--- tests/001-run-jobs.sh
+++ tests/001-run-jobs.sh
@@ -17,7 +17,10 @@ token=$(echo ${login##*token\" : \"} | cut -d '"' -f 1)
 # Test - run the jobs
 echo "Running test jobs"
 # curl -k -s -H "Authorization: Bearer $token" -X POST -F "definitionsFile=@../ctmjobs/MFT-conn-profiles.json" "$endpoint/deploy"
-submit=$(curl -k -s -H "Authorization: Bearer $token" -X POST -F "jobDefinitionsFile=@../ctmjobs/jobs.json" "$endpoint/run")
+# submit=$(curl -k -s -H "Authorization: Bearer $token" -X POST -F "jobDefinitionsFile=@../ctmjobs/jobs.json" "$endpoint/run")
+submit=$(curl -k -s -H "Authorization: Bearer $token" -X POST -F "jobDefinitionsFile=@$1" "$endpoint/run")
+
+
 runid=$(echo ${submit##*runId\" : \"} | cut -d '"' -f 1)
 
 # Check job status
